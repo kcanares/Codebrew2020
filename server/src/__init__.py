0cf8af5cbe40a22bac58ce489880448addfcf133
An Nguyen <anthailan1702@gmail.com>
2020-09-26 00:11:25 +1000

    Add scraper and recipe API

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -5,14 +5,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_mongoengine import MongoEngine
+from pymongo import MongoClient
 
 from .config import Config
 
-db = MongoEngine()
 app = Flask(__name__)
 CORS(app)
 app.config.from_object(Config)
-db.init_app(app)
+
+
+client = MongoClient('mongodb://165.22.254.55:27017/')
+db = client['codebrew2020']
 
 from .routes import *
 from .models import *
--- server/src/models/users.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-from src import db
-
-class User(db.Document):
-    name = db.StringField(max_length=200)
-    created = db.DateTimeField(default=datetime.datetime.now)
