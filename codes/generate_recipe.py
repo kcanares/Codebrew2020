187fb5c3911f581ea6fcc360f71ad1e6bdada3a5
Kyla C <canares.kyla@gmail.com>
2020-09-26 17:18:53 +1000

    updated matching algo

--- codes/generate_recipe.py
+++ codes/generate_recipe.py
@@ -2,7 +2,11 @@ import pymongo
 from pymongo import MongoClient
 from pandas import DataFrame
 import numpy as np
+from bson.objectid import ObjectId
 
+DAYS_IN_A_WEEK = 7
+
+""""matches recipes to ingredients. sorts them according to match ratio"""
 def generate_recipe(user_id):
     client = MongoClient("mongodb://165.22.254.55:27017/")
     db = client['codebrew2020']
@@ -13,40 +17,64 @@ def generate_recipe(user_id):
 
     products = product_collection.find({})
     recipes = recipe_collection.find({})
-    user = user_collection.find_one({'_id': user_id})
+    user = user_collection.find_one({'_id': ObjectId('5f6e7fdf20993e3adbef5535')})
+    print(user)
 
     recipe_lst = list(recipes)
     recipe_df = DataFrame(recipe_lst)
     recipe_df["ratio"] = np.nan
 
     # create a column of empty lists
+    recipe_df["sale ingredient ids"] = np.empty((len(recipe_df), 0)).tolist()
     recipe_df["sale ingredients"] = np.empty((len(recipe_df), 0)).tolist()
-    product_lst =list(products)
+    recipe_df["not on sale ingredients"] = np.empty((len(recipe_df), 0)).tolist()
 
+    product_lst =list(products)
     for i in range(len(recipe_df)):
+        print(i)
         for ingredient in recipe_df.iloc[i]['ingredients']:
             # TODO: check allergy
             # if ingredient['name'] in user['dietary_req']:
             #     recipe_info.pop(recipe['_id'])
             #     break
 
-            # check discount
+            ingredient_words = ingredient['name'].split()
+            if "stock" or "noodles" in ingredient_words:
+                ingredient_words = [ingredient['name']]
+
+            # iterate through products and match records with ingredient database
             for product in product_lst:
-                for word in ingredient['name'].split():
-                    if word in product['title']:
-                        recipe_df.iloc[i]["sale ingredients"].append(product['_id'])
-        ratio = float(len(recipe_df.loc[i]["sale ingredients"]) / len(recipe_df.loc[i]['ingredients']))
-        recipe_df.loc[i]['ratio'] = ratio
+                match_found = False
+                for word in ingredient_words:
+                    if word in product['tags']:
+                        print(product['title'], ingredient['name'], product['tags'])
+                        recipe_df.iloc[i]["sale ingredient ids"].append(product['_id'])
+                        recipe_df.iloc[i]["sale ingredients"].append(product['title'])
+                        match_found = True
+                        break
+                if match_found:
+                    break
+            if not match_found:
+                recipe_df.iloc[i]["not on sale ingredients"].append(ingredient['name'])
 
-    recipe_df.sort_values(by=['ratio'])
-    print(recipe_df.iloc[:]['ratio'])
-    # # return top 10
-    # top_10_list = []
-    # highest = 10
-    # for i in range(0, highest):
-    #     top_10_list.append(recipe_info[top_10_recipe[i]['_id']])
-    #
-    # return top_10_list
+        recipe_df.loc[i, 'ratio'] = \
+            len(recipe_df.loc[i]["sale ingredient ids"]) / len(recipe_df.iloc[i]['ingredients'])
 
 
-print(generate_recipe('5f6e7fdf20993e3adbef5535'))
+    recipe_df = recipe_df.sort_values(by=['ratio'], ascending=False)
+    # for debugging
+    recipe_df.to_csv("match.csv")
+
+    meal_no = calculate_user_meal_no(user)["meals"]
+    return recipe_df.iloc[:, meal_no]
+
+def calculate_user_meal_no(user):
+    if user['dietary_requirements'] == "Everything":
+        return {"breakfast": 1, "meals": 2 * DAYS_IN_A_WEEK}
+
+def tally_ingredients(recipe_df):
+     for
+
+recipe_df = generate_recipe('5f6e7fdf20993e3adbef5535')
+tally_ingredients(recipe_df)
+print(recipe_df)
