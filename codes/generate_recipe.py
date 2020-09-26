edce81d7e00a554be77598c3936ab10c43b8a0a4
Kyla C <canares.kyla@gmail.com>
2020-09-26 20:28:47 +1000

    fixed messed up return on generate recipe function. Added more comments to ingredient compile function

--- codes/generate_recipe.py
+++ codes/generate_recipe.py
@@ -8,8 +8,10 @@ from collections import defaultdict
 
 DAYS_IN_A_WEEK = 7
 
-""""matches recipes to ingredients. sorts them according to match ratio"""
 def generate_recipe(user_id):
+    """"matches recipes to ingredients. sorts them according to match ratio"""
+
+    # connect to client and get collections
     client = MongoClient("mongodb://165.22.254.55:27017/")
     db = client['codebrew2020']
 
@@ -20,7 +22,6 @@ def generate_recipe(user_id):
     products = product_collection.find({})
     recipes = recipe_collection.find({})
     user = user_collection.find_one({'_id': ObjectId('5f6e7fdf20993e3adbef5535')})
-    print(user)
 
     recipe_lst = list(recipes)
     recipe_df = DataFrame(recipe_lst)
@@ -33,7 +34,6 @@ def generate_recipe(user_id):
 
     product_lst =list(products)
     for i in range(len(recipe_df)):
-        print(i)
         for ingredient in recipe_df.iloc[i]['ingredients']:
             # TODO: check allergy
             # if ingredient['name'] in user['dietary_req']:
@@ -49,7 +49,7 @@ def generate_recipe(user_id):
                 match_found = False
                 for word in ingredient_words:
                     if word in product['tags']:
-                        print(product['title'], ingredient['name'], product['tags'])
+                        # print(product['title'], ingredient['name'], product['tags'])
                         recipe_df.iloc[i]["sale ingredient ids"].append(product['_id'])
                         recipe_df.iloc[i]["sale ingredients"].append({"id": product['_id'],
                                                                       "quantity": ingredient['name'],
@@ -59,32 +59,36 @@ def generate_recipe(user_id):
                 if match_found:
                     break
             if not match_found:
-                recipe_df.iloc[i]["non sale ingredients"].append(ingredient['name'])
+                recipe_df.iloc[i]["non sale ingredients"].append(ingredient)
 
         recipe_df.loc[i, 'ratio'] = \
             len(recipe_df.loc[i]["sale ingredient ids"]) / len(recipe_df.iloc[i]['ingredients'])
-
 
     recipe_df = recipe_df.sort_values(by=['ratio'], ascending=False)
     # for debugging
     recipe_df.to_csv("match.csv")
 
-    meal_no = calculate_user_meal_no(user)["meals"]
-    return recipe_df.iloc[:, meal_no]
+    # meal_no = calculate_user_meal_no(user)["meals"]
+    meal_no = 14
+    return recipe_df.iloc[:meal_no][:].reset_index(drop=True)
+
 
 def calculate_user_meal_no(user):
     if user['dietary_requirements'] == "Everything":
         return {"breakfast": 1, "meals": 2 * DAYS_IN_A_WEEK}
 
+
 def tally_ingredients(recipe_df):
-    """compiles all of the ingredients into two dictionaries (faster than growing a dataframe). There will be the sale
-    and non-sale dictionary."""
+    """Pass in df of week's recipes. Compiles all of the ingredients into two dictionaries (faster than growing a
+    dataframe). There will be the sale and non-sale dictionary. Note: format for sale_dict is:
+    {('ingredient, 'measurement'): {'store': ,  'discount': , 'quantity': }}"""
+
     non_sale_dict = defaultdict(dict)
     savings = 0
-
+    print(recipe_df.columns)
     for i in range(len(recipe_df)):
-        for ingredient in recipe_df['non sale ingredients']:
-            non_sale_dict['name']['measurement'] += ingredient['quantity']
+        for ingredient in recipe_df.loc[i]['non sale ingredients']:
+            non_sale_dict[ingredient["name"], ingredient["measurement"]] += ingredient['quantity']
 
     print(non_sale_dict)
 
@@ -93,6 +97,8 @@ def find_breakfast():
 """
 
 recipe_df = generate_recipe('5f6e7fdf20993e3adbef5535')
+
+recipe_df.to_csv("week recipes.csv")
 tally_ingredients(recipe_df)
-print(recipe_df)
+
 print(tally_ingredients(recipe_df))
