349a51952f685abb5b6fcf9b63073f6ab4ada7d7
Kyla C <canares.kyla@gmail.com>
2020-09-26 19:07:33 +1000

    add functionality to collate ingredients

--- codes/generate_recipe.py
+++ codes/generate_recipe.py
@@ -1,8 +1,10 @@
 import pymongo
 from pymongo import MongoClient
+import pandas as pd
 from pandas import DataFrame
 import numpy as np
 from bson.objectid import ObjectId
+from collections import defaultdict
 
 DAYS_IN_A_WEEK = 7
 
@@ -27,7 +29,7 @@ def generate_recipe(user_id):
     # create a column of empty lists
     recipe_df["sale ingredient ids"] = np.empty((len(recipe_df), 0)).tolist()
     recipe_df["sale ingredients"] = np.empty((len(recipe_df), 0)).tolist()
-    recipe_df["not on sale ingredients"] = np.empty((len(recipe_df), 0)).tolist()
+    recipe_df["non sale ingredients"] = np.empty((len(recipe_df), 0)).tolist()
 
     product_lst =list(products)
     for i in range(len(recipe_df)):
@@ -49,13 +51,15 @@ def generate_recipe(user_id):
                     if word in product['tags']:
                         print(product['title'], ingredient['name'], product['tags'])
                         recipe_df.iloc[i]["sale ingredient ids"].append(product['_id'])
-                        recipe_df.iloc[i]["sale ingredients"].append(product['title'])
+                        recipe_df.iloc[i]["sale ingredients"].append({"id": product['_id'],
+                                                                      "quantity": ingredient['name'],
+                                                                      "measurement": ingredient['measurement']})
                         match_found = True
                         break
                 if match_found:
                     break
             if not match_found:
-                recipe_df.iloc[i]["not on sale ingredients"].append(ingredient['name'])
+                recipe_df.iloc[i]["non sale ingredients"].append(ingredient['name'])
 
         recipe_df.loc[i, 'ratio'] = \
             len(recipe_df.loc[i]["sale ingredient ids"]) / len(recipe_df.iloc[i]['ingredients'])
@@ -73,8 +77,22 @@ def calculate_user_meal_no(user):
         return {"breakfast": 1, "meals": 2 * DAYS_IN_A_WEEK}
 
 def tally_ingredients(recipe_df):
-     for
+    """compiles all of the ingredients into two dictionaries (faster than growing a dataframe). There will be the sale
+    and non-sale dictionary."""
+    non_sale_dict = defaultdict(dict)
+    savings = 0
+
+    for i in range(len(recipe_df)):
+        for ingredient in recipe_df['non sale ingredients']:
+            non_sale_dict['name']['measurement'] += ingredient['quantity']
+
+    print(non_sale_dict)
+
+"""
+def find_breakfast():
+"""
 
 recipe_df = generate_recipe('5f6e7fdf20993e3adbef5535')
 tally_ingredients(recipe_df)
 print(recipe_df)
+print(tally_ingredients(recipe_df))
